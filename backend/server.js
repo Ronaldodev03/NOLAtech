e9f6907210efb8868f692a02775510016f1c61db
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-11-05 04:13:48 -0400

    adding routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 import { ENV_VARS } from "./config/envVars.js";
 
 import authRoutes from "./routes/auth.route.js";
+import evaluationRoutes from "./routes/evaluation.route.js";
+import feedbackRoutes from "./routes/feedback.route.js";
+import reportsRoutes from "./routes/reports.route.js";
+import userRoutes from "./routes/user.route.js";
+//import { errorHandler } from './middleware/error.middleware.js';
 
 import { connectDB } from "./config/db.js";
 
@@ -17,6 +22,13 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/evaluations", evaluationRoutes);
+app.use("/api/feedback", feedbackRoutes);
+app.use("/api/reports", reportsRoutes);
+
+// Error handling middleware
+//app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
