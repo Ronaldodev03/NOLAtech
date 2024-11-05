1ce32bb0b01190c9b4df7a78af063010dc2c6e6b
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-11-05 05:06:32 -0400

    configuration for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,15 @@ import reportsRoutes from "./routes/reports.route.js";
 import userRoutes from "./routes/user.route.js";
 //import { errorHandler } from './middleware/error.middleware.js';
 
+import path from "path";
+
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
 
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -26,6 +29,14 @@ app.use("/api/users", userRoutes);
 app.use("/api/evaluations", evaluationRoutes);
 app.use("/api/feedback", feedbackRoutes);
 app.use("/api/reports", reportsRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // Error handling middleware
 //app.use(errorHandler);
