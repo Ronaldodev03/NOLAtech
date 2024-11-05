630a52a2c9236f08638667f0e40b199b6601bca7
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-11-05 14:45:30 -0400

    feedback page

---
+++ frontend/src/pages/FeedbackPage.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+const FeedbackPage = () => {
+  return (
+    <Card>
+      <CardContent className=" py-6 text-center">
+        Not implemented yet!
+      </CardContent>
+    </Card>
+  );
+};
+
+export default FeedbackPage;
