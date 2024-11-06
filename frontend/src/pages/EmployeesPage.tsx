426614cd6c3c5e6480b6109ace7b30a2a7855156
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-11-05 22:01:15 -0400

    no mostrar columna de evaluar si es user Employee

--- frontend/src/pages/EmployeesPage.tsx
+++ frontend/src/pages/EmployeesPage.tsx
@@ -1,10 +1,18 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DataTable } from "@/components/data-table";
-import { columns } from "@/components/Employee/columns";
+import { columns as baseColumns } from "@/components/Employee/columns";
 import { useUserStore } from "@/stores/userStore";
+import { useAuthStore } from "@/stores/authUser";
 
 export default function EmployeesPage() {
   const { employees } = useUserStore();
+
+  const { user } = useAuthStore();
+
+  const filteredColumns =
+    user?.role === "Employee"
+      ? baseColumns.filter((column) => column.header !== "Evaluate")
+      : baseColumns;
 
   return (
     <Card>
@@ -12,7 +20,7 @@ export default function EmployeesPage() {
         <CardTitle>Employees</CardTitle>
       </CardHeader>
       <CardContent>
-        {employees && <DataTable columns={columns} data={employees} />}
+        {employees && <DataTable columns={filteredColumns} data={employees} />}
       </CardContent>
     </Card>
   );
